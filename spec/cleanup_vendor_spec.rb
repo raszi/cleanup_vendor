ca15e37066813c294f47c8b2806f5fd3b16d8c1f
KARASZI István <github@spam.raszi.hu>
2020-03-14 09:05:05 +0100

    Use a temporary directory in filter tests

--- spec/cleanup_vendor_spec.rb
+++ spec/cleanup_vendor_spec.rb
@@ -1,13 +1,13 @@
 require 'tempfile'
 
 RSpec.describe CleanupVendor do
+  let(:dir) { Pathname.new(Dir.mktmpdir('cleanup_vendor')) }
+
   it 'has a version number' do
     expect(CleanupVendor::VERSION).not_to be nil
   end
 
   describe '.run' do
-    let(:dir) { Pathname.new(Dir.mktmpdir('cleanup_vendor')) }
-
     let!(:extensions) do
       %w[c cpp gem h hpp java log md mk o rdoc txt].map do |ext|
         file = Tempfile.create(['test', ".#{ext}"], Dir.mktmpdir('kept', dir))
@@ -90,12 +90,14 @@ RSpec.describe CleanupVendor do
       expect { described_class.filter(__FILE__) }.to raise_error(CleanupVendor::Error)
     end
 
-    it 'should return an empty list by default' do
-      expect { described_class.filter('.').to be_empty }
-    end
-
     context 'when called with a real directory' do
-      let(:dir) { File.expand_path('../..', __FILE__) }
+      let!(:rb) { Tempfile.create(['test', '.rb'], dir) }
+      let!(:spec) { Dir.mkdir(File.join(dir, 'spec')) }
+      let!(:fix_filename) { File.basename(Tempfile.create('test', dir).path) }
+
+      it 'without options it should return with an empty list' do
+        expect { described_class.filter(dir).to be_empty }
+      end
 
       it { expect { |b| described_class.filter(dir, extensions: %w[rb], &b) }.to yield_control }
 
@@ -108,11 +110,11 @@ RSpec.describe CleanupVendor do
       it 'should filter for directories' do
         entries = described_class.filter(dir, directories: %w[spec])
 
-        expect(entries).to all(satisfy { |f| File.directory?(f) && %w[spec .].include?(File.basename(f)) })
+        expect(entries).to all(satisfy { |f| File.directory?(f) && %w[spec].include?(File.basename(f)) })
       end
 
       it 'should filter for filenames' do
-        expect { |b| described_class.filter(dir, filenames: %w[cleanup_vendor.gemspec], &b) }.to yield_with_args('cleanup_vendor.gemspec')
+        expect { |b| described_class.filter(dir, filenames: [fix_filename], &b) }.to yield_with_args(fix_filename)
       end
     end
   end
