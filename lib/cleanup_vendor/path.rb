bd424768d607c09784a5bed5a0b492ec9feadd81
KARASZI István <ikaraszi@gmail.com>
2020-12-08 11:04:53 +0100

    Fix RuboCop warning

--- lib/cleanup_vendor/path.rb
+++ lib/cleanup_vendor/path.rb
@@ -14,9 +14,9 @@ module CleanupVendor
 
     def match?(patterns)
       patterns.any? do |p|
-        p == self.to_s || # support exact matches
-        p.start_with?('**') && fnmatch?(p, File::FNM_EXTGLOB) || 
-        basename.fnmatch?(p, File::FNM_EXTGLOB) && gem_level?
+        p.eql?(self) ||
+          p.start_with?('**') && fnmatch?(p, File::FNM_EXTGLOB) ||
+          basename.fnmatch?(p, File::FNM_EXTGLOB) && gem_level?
       end
     end
 
